fdf131b8b2a1f8ac4cfaa8c11977917147592775
Daniel Martin <danielt8642@gmail.com>
2019-01-24 21:42:37 -0800

    Exercised handlebars partials and helpers

--- server.js
+++ server.js
@@ -3,22 +3,29 @@ const hbs = require("hbs");
 
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+
 app.get("/", (req, res) => {
   res.render("homepage.hbs", {
-    homePageTitle: "Home",
+    pageTitle: "Home",
     welcomeMessage:
-      "Welcome! Today is the start of something new, something big!",
-    currentYear: new Date().getFullYear()
+      "Welcome! Today is the start of something new, something big!"
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about.hbs", {
-    pageTitle: "About Page",
-    currentYear: new Date().getFullYear()
+    pageTitle: "About Page"
   });
 });
 
