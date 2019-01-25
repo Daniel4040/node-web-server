e2ea3716ad41de9f60725372b6993ae172049366
Daniel Martin <danielt8642@gmail.com>
2019-01-24 19:35:48 -0800

    Exercise: basic handlebars webpage

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const hbs = require("hbs");
+
+var app = express();
+
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("homepage.hbs", {
+    homePageTitle: "Home",
+    welcomeMessage:
+      "Welcome! Today is the start of something new, something big!",
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page",
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "Bad request"
+  });
+});
+
+// /bad - send back json with error message
+
+app.listen(3000, () => {
+  console.log("Server up on port 3000");
+});
